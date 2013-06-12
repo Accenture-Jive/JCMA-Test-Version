b2e858833d311673798edef4623641661fc9f024
Ripul Thakur <ripul.thakur@accenture.com>
2013-06-12 17:23:57 +0530

    commit changes

--- js/movendelete.js
+++ js/movendelete.js
@@ -393,8 +393,8 @@ else {
 		$("#cmdu").hide();
 		$("#button_div").show();
 		$("#src_place").show();
-		$('#selected_items').css("margin-top", "-105px");
-		$('#button_div').css("margin-top", "10px");
+		//$('#selected_items').css("margin-top", "-105px");
+		//$('#button_div').css("margin-top", "10px");
 		$("#select_items_button").show();
 		var str='Moving completed. Please click   <a href='+finalurl+'>here </a>  for the new location of your content.';
 		document.getElementById("frame1").contentDocument.body.innerHTML = "Note.<br><br><span id='mySpan' style='font-weight:bold;'>"+str.fontcolor("#3778C7")+"</span>";

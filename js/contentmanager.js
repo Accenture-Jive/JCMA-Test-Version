97aa65cda925ab52f396833172736766f146021e
Ripul Thakur <ripul.thakur@accenture.com>
2013-06-13 20:43:25 +0530

    commit changes

--- js/contentmanager.js
+++ js/contentmanager.js
@@ -5647,7 +5647,7 @@ $("#finale_tag_sel").show();
 $("#finale_tag_place").show();
 //$("#finale_tag_sel").css("margin-top","70px");
 $("#finale_add_tag_button").show();
-//$("#finale_add_tag_button").css("margin-top","65px");
+$("#finalSelTag").css("margin-top","10px");
 $("#finale_add_tag").hide();
 $("#finale_tag_select_items_button").hide();
 $("#finale_finalTagTo").hide();
